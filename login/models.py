d52442c7375d20b1c5304bbd29fd5fccd9ecd731
James Urban <jurban34@gmail.com>
2019-12-31 01:06:25 -0800

    More cleanup

--- login/models.py
+++ login/models.py
@@ -5,15 +5,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    
 
-    email = models.EmailField(default='', primary_key=True)
-    hashed_password = models.CharField(max_length=200, default='')
-    name = models.CharField(max_length=200, default='')
-
-    @classmethod
-    def create_user(self, email, plain_text_password, name):
-        hashed_password = make_password(plain_text_password)
-        User.objects.create(email=email, hashed_password=hashed_password, name=name)
-
-#class UserPreferences(models.Model):
-#    league = models.C
